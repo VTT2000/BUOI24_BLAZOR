a1988966043ed5479744083425d844a701ec130d
VuuThuan <thuancmag2000@gmail.com>
2024-12-26 21:37:32 +0700

    buoi4

--- web_blazor_server/obj/Debug/net8.0/web_blazor_server.AssemblyInfo.cs
+++ web_blazor_server/obj/Debug/net8.0/web_blazor_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a45c1806507dbb567d4a9d59f1f4781e0e8b195")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3477261950b730c04f0e315060349cea07af1b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
