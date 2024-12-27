36ce5c50c6a3c4c95442f33865f3ae955d526e49
VuuThuan <thuancmag2000@gmail.com>
2024-12-27 16:29:33 +0700

    changeQuantitychangeQuantity

--- web_blazor_server/obj/Debug/net8.0/web_blazor_server.AssemblyInfo.cs
+++ web_blazor_server/obj/Debug/net8.0/web_blazor_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1988966043ed5479744083425d844a701ec130d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c68ec255a15c231a169bb7e5dcfda7c48ac08e")]
 [assembly: System.Reflection.AssemblyProductAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web_blazor_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
