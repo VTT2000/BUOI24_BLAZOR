c791fb10bcb096a5daeea8b5bcf36de23463b284
VuuThuan <thuancmag2000@gmail.com>
2024-12-31 21:47:45 +0700

    buoi5buoi5

---
+++ web_blazor_server/Models/ProductModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace web_blazor_server.Models;
+
+public class ProductModel
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("price")]
+    public double Price { get; set; }
+
+    [JsonPropertyName("img")]
+    public Uri Img { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("deleted")]
+    public bool Deleted { get; set; }
+}
